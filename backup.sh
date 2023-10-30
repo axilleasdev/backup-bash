16e4b4886d42aacb8c8440af79981489fe8e8259
Achilleas Dev <99798880+axilleasdev@users.noreply.github.com>
2023-10-30 22:42:25 +0200

    Update backup.sh
    New optimised version

--- backup.sh
+++ backup.sh
@@ -1,71 +1,44 @@
 #!/bin/bash
 
-# This checks if the number of arguments is correct
-# If the number of arguments is incorrect ( $# != 2) print error message and exit
-if [[ $# != 2 ]]
-then
-  echo "backup.sh target_directory_name destination_directory_name"
-  exit
+# Check the number of arguments
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 source_directory destination_directory"
+  exit 1
 fi
 
-# This checks if argument 1 and argument 2 are valid directory paths
-if [[ ! -d $1 ]] || [[ ! -d $2 ]]
-then
+sourceDir="$1"
+destDir="$2"
+
+# Check if the source and destination directories exist
+if [ ! -d "$sourceDir" ] || [ ! -d "$destDir" ]; then
   echo "Invalid directory path provided"
-  exit
+  exit 1
 fi
 
-# [TASK 1]
-targetDirectory=$1
-destinationDirectory=$2
-
-# [TASK 2]
-echo "Target Directory is : $targetDirectory"
-echo "Destination Directory: $destinationDirectory"
-
-# [TASK 3]
+# Get current timestamp
 currentTS=$(date '+%s')
 
-# [TASK 4]
+# Create backup filename
 backupFileName="backup-$currentTS.tar.gz"
 
-# We're going to:
-  # 1: Go into the target directory
-  # 2: Create the backup file
-  # 3: Move the backup file to the destination directory
+# Move to the source directory
+cd "$sourceDir" || exit 1
 
-# To make things easier, we will define some useful variables...
-
-# [TASK 5]
-origAbsPath=$(pwd)
-
-# [TASK 6]
-cd "$destinationDirectory"
-destAbsPath=$(pwd)
-
-# [TASK 7]
-cd "$origAbsPath"
-cd "$targetDirectory"
-
-# [TASK 8]
+# Get the list of files modified in the last 24 hours
+toBackup=()
 yesterdayTS=$((currentTS - 24 * 60 * 60))
 
-declare -a toBackup
-
-for file in $(ls) # [TASK 9]
-do
-  # [TASK 10]
-  if (( $(date -r "$file" +%s) > yesterdayTS ))
-  then
-    # [TASK 11]
+for file in *; do
+  fileTS=$(date -r "$file" '+%s")
+  if [ "$fileTS" -gt "$yesterdayTS" ]; then
     toBackup+=("$file")
   fi
 done
 
-# [TASK 12]
+# Create a tarball of the selected files
 tar -czvf "$backupFileName" "${toBackup[@]}"
 
-# [TASK 13]
-mv "$backupFileName" "$destAbsPath"
+# Move the backup file to the destination directory
+mv "$backupFileName" "$destDir"
 
-# Congratulations! You completed the final project for this course!
+echo "Backup completed successfully: $destDir/$backupFileName"
